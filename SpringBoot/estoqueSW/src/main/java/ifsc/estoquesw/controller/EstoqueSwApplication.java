ad2b1cded351adc0d96048ccb1f54f77da44d95b
Aluno <Gabriel.souza.0702@gmail.com>
2026-03-13 16:05:25 -0300

    atualizando ifsc

---
+++ SpringBoot/estoqueSW/src/main/java/ifsc/estoquesw/controller/EstoqueSwApplication.java
@@ -0,0 +1,13 @@
+package ifsc.estoquesw.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueSwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstoqueSwApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/estoqueSW/src/test/java/ifsc/estoquesw/EstoqueSwApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsc.estoquesw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueSwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
