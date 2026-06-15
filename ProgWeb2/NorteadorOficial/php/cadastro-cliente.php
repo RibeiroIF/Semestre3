e15898b229a5b1b52d377ee9b5951e6cadf14506
Aluno <Gabriel.souza.0702@gmail.com>
2026-06-15 16:03:06 -0300

    ifsc 15/06

--- ProgWeb2/NorteadorOficial/php/cadastro-cliente.php
+++ ProgWeb2/NorteadorOficial/php/cadastro-cliente.php
@@ -15,10 +15,10 @@
           <h1 class="title"> LavaCar Aquaman </h1>
             <nav class="navbar">
               <ul>
-                <li> <a href="homepage.html" class="card" id="return"> Voltar à Página Inicial </a> </li>
-                <li> <a href="cadastro-veiculo.html" class="card"> Cadastrar Veículo </a> </li>
-                <li> <a href="login-cliente.html" class="card"> Login do Cliente </a> </li>
-                <li> <a href="login-admin.html" class="card"> Login Administrador </a> </li>
+                <li> <a href="homepage.php" class="card" id="return"> Voltar à Página Inicial </a> </li>
+                <li> <a href="cadastro-veiculo.php" class="card"> Cadastrar Veículo </a> </li>
+                <li> <a href="login-cliente.php" class="card"> Login do Cliente </a> </li>
+                <li> <a href="login-admin.php" class="card"> Login Administrador </a> </li>
               </ul>
             </nav>
         </header>
@@ -27,28 +27,28 @@
         <div class="espaco-imagem">
           <div class="slider">
             <div class="display">
-              <img src="/imagens/lavacao-imagem-1.jpg" alt="foto lavação externa">
-              <img src="/imagens/lavacao-imagem-2.jpg" alt="foto lavação completa">
-              <img src="/imagens/lavacao-imagem-3.jpeg" alt="foto enceramento">
-              <img src="/imagens/lavacao-imagem-4.jpg" alt="foto lavação interna">
-              <img src="/imagens/lavacao-imagem-5.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-6.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-7.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-8.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-9.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-10.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-1.jpg" alt="foto lavação externa">
+              <img src="../imagens/lavacao-imagem-2.jpg" alt="foto lavação completa">
+              <img src="../imagens/lavacao-imagem-3.jpeg" alt="foto enceramento">
+              <img src="../imagens/lavacao-imagem-4.jpg" alt="foto lavação interna">
+              <img src="../imagens/lavacao-imagem-5.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-6.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-7.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-8.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-9.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-10.jpeg" alt="foto lavação">
             </div>
             <div aria hidden class="display">
-              <img src="/imagens/lavacao-imagem-1.jpg" alt="foto lavação externa">
-              <img src="/imagens/lavacao-imagem-2.jpg" alt="foto lavação completa">
-              <img src="/imagens/lavacao-imagem-3.jpeg" alt="foto enceramento">
-              <img src="/imagens/lavacao-imagem-4.jpg" alt="foto lavação interna">
-              <img src="/imagens/lavacao-imagem-5.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-6.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-7.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-8.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-9.jpeg" alt="foto lavação">
-              <img src="/imagens/lavacao-imagem-10.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-1.jpg" alt="foto lavação externa">
+              <img src="../imagens/lavacao-imagem-2.jpg" alt="foto lavação completa">
+              <img src="../imagens/lavacao-imagem-3.jpeg" alt="foto enceramento">
+              <img src="../imagens/lavacao-imagem-4.jpg" alt="foto lavação interna">
+              <img src="../imagens/lavacao-imagem-5.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-6.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-7.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-8.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-9.jpeg" alt="foto lavação">
+              <img src="../imagens/lavacao-imagem-10.jpeg" alt="foto lavação">
             </div>
           </div>
         </div>
